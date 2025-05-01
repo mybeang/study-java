d958150b004d7b36f9f89bee01318d0697e08e04
mybeang <mybeang@gmail.com>
2025-05-01 16:06:18 +0900

    20250501 java study first chapter

---
+++ JavaWorksapce/src/chap_01/_04_Comments.java
@@ -0,0 +1,19 @@
+package chap_01;
+
+public class _04_Comments {
+    public static void main(String[] args) {
+        System.out.println("(10분 전) 잠시 후 결혼식 시작 예정이오니 착성 부탁드립니다.");
+        // System.out.println("(5분 전) 잠시 후 결혼식 시작 예정이오니 착성 부탁드립니다.");
+        System.out.println("지금부터 게임을 시작하지.");
+
+        int size = 120;
+        size += 10;  // 애들은 발이 잘 크잖아
+        System.out.println("신발 사이즈는 " + size + " 입니다.");
+
+        /*멀티 스트링용 주석은 이렇게 적는거지 아마도
+        재밌구먼
+        정말인가
+        나도몰러
+        살려줘*/
+    }
+}
---
+++ JavaWorksapce/src/chap_01/_07_TypeCasting.java
@@ -0,0 +1,44 @@
+package chap_01;
+
+public class _07_TypeCasting {
+    public static void main(String[] args) {
+        // 형변환 TypeCasting
+        // 정수형 -> 실수형 / 실수형 -> 정수형
+
+        // int to double (or float)
+        int score = 93;
+        System.out.println(score); // 93
+        System.out.println((double) score); // 93.0
+
+        // double (or float) to int
+        float score_f = 93.3F;
+        double score_d = 98.8;
+        System.out.println((int) score_f);
+        System.out.println((int) score_d);
+
+        score = 93 + (int) 98.8;
+        System.out.println(score);
+        score_d = 93 + 98.8;
+        System.out.println(score_d);
+
+        double convertedScoreDouble = score;  // 자동으로됨
+        System.out.println(convertedScoreDouble);
+
+        int convertedScoreInt = (int) score_d;  // 수동으로 해야됨
+        System.out.println(convertedScoreInt);
+
+        long longNo = 1_000_000_000_000_000L;
+        System.out.println(longNo);  // 1000000000000000
+        System.out.println((int) longNo);  // -1530494976
+
+        int s1 = 93;
+        System.out.println(s1);
+//        String s2 = String.valueOf(s1);
+        String s2 = Integer.toString(s1 + 3);
+        System.out.println(s2);
+        int s3 = Integer.parseInt(s2 + 1);
+        System.out.println(s3);
+
+        int error = Integer.parseInt("asdf");
+    }
+}
