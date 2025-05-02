4b8ebad371952d5495b4a3546b2a56a0544b7bb6
mybeang <mybeang@gmail.com>
2025-05-02 22:28:01 +0900

    chapter 2

---
+++ JavaWorksapce/src/chap_02/_04_Operator4.java
@@ -0,0 +1,26 @@
+package chap_02;
+
+public class _04_Operator4 {
+    public static void main(String[] args) {
+        // 논리 연산자
+        boolean kimchi = true;
+        boolean egg = true;
+        boolean meat = true;
+
+        System.out.println(kimchi || egg || meat);
+        System.out.println(kimchi && egg && meat);
+        egg = false;
+        System.out.println(kimchi || egg || meat);
+        System.out.println(kimchi && egg && meat);
+
+        System.out.println((5 > 3) && (3 > 1));
+        System.out.println((5 > 3) && (3 < 1));
+
+        System.out.println((5 > 3) || (3 > 1));
+        System.out.println((5 > 3) || (3 < 1));
+        System.out.println((5 < 3) || (3 < 1));
+
+        System.out.println(!true);
+        System.out.println(!false);
+    }
+}
