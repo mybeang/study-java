8f497fefd837de69695adb860fd8344080636279
mybeang <mybeang@gmail.com>
2025-05-05 16:21:50 +0900

    chapter 4

---
+++ JavaWorksapce/src/chap_04/_02_Else.java
@@ -0,0 +1,23 @@
+package chap_04;
+
+public class _02_Else {
+    public static void main(String[] args) {
+        int hour = 15;
+        if (hour > 13) {
+            System.out.println("전반전) 커피 안마셔");
+        } else {
+            System.out.println("전반전) 커피 마실래");
+        }
+        System.out.println("전반전) 예얍");
+
+        boolean morningCoffee = true;
+        if (hour > 13 && morningCoffee) {
+            System.out.println("후반전) 커피 안마셔");
+        } else if (hour > 13 || morningCoffee) {
+            System.out.println("후반전) 디카페인 커피로 마실래");
+        } else {
+            System.out.println("후반전) 커피 마실래");
+        }
+        System.out.println("후반전) 예얍");
+    }
+}
---
+++ JavaWorksapce/src/chap_04/_07_DoWhile.java
@@ -0,0 +1,23 @@
+package chap_04;
+
+public class _07_DoWhile {
+    public static void main(String[] args) {
+        int move = 0;
+        int distance = 25;
+        int height = 25;
+
+        System.out.println("-- while --");
+        while (move + height < distance) {
+            System.out.println("현재 이동 거리: " + (move + height));
+            move += 3;
+        }
+        System.out.println("도착!");
+        System.out.println("-- do while --");
+        move = 0;
+        do {
+            System.out.println("현재 이동 거리: " + (move + height));
+            move += 3;
+        } while (move + height < distance);
+        System.out.println("도착!");
+    }
+}
