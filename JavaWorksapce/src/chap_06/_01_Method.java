2264002d7a340942bbbbb8c243585a2afca2b5dc
mybeang <mybeang@gmail.com>
2025-05-06 16:29:58 +0900

    chapter 06

---
+++ JavaWorksapce/src/chap_06/_01_Method.java
@@ -0,0 +1,18 @@
+package chap_06;
+
+public class _01_Method {
+    public static void sayHello() {
+        System.out.println("Hello world!!!");
+    };
+
+    public static int sum(int x, int y) {
+        return x + y;
+    };
+
+    public static void main(String[] args) {
+        sayHello();
+        System.out.println(sum(1, 3));
+        System.out.println(sum(10, 31));
+        System.out.println(sum(11231, 3556));
+    }
+}
