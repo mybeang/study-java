a79b6f601b7d2b0b8e2c876e6d9b964f8014d5b1
mybeang <mybeang@gmail.com>
2025-05-10 14:40:25 +0900

    start spring boot

---
+++ mybeang-home01-spring/mybeang-home01-spring/src/main/java/mybeang/home01/mybeang_home01_spring/MybeangHome01SpringApplication.java
@@ -0,0 +1,13 @@
+package mybeang.home01.mybeang_home01_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybeangHome01SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybeangHome01SpringApplication.class, args);
+	}
+
+}
---
+++ mybeang-home01-spring/mybeang-home01-spring/src/test/java/mybeang/home01/mybeang_home01_spring/MybeangHome01SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mybeang.home01.mybeang_home01_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybeangHome01SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
