e85aeff71acb68d1a5620967e4af085298240790
mybeang <mybeang@gmail.com>
2025-05-13 17:24:25 +0900

    spring data jpa

--- mybeang-home01-spring/src/main/java/mybeang/home01/mybeang_home01_spring/SpringConfig.java
+++ mybeang-home01-spring/src/main/java/mybeang/home01/mybeang_home01_spring/SpringConfig.java
@@ -11,21 +11,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    private EntityManager em;
+//    private EntityManager em;
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-        return new MySqlMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+//        return new MySqlMemberRepository(em);
+//    }
 }
